dbe57378f0402c20cf72b7964d686875b3481b77
sophie9292 <shuyen19920128@gmail.com>
2015-08-23 17:14:20 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+# makeCacheMatrix stores the value of the "input_matrix" and compute 
+# the inverse of the "input_matrix"
+# as the input_matrix change, as long as cache with makeCacheMatrix,
+# e.g. time1 <- makeCacheMatrix("input_matrix")
+# CacheSolve(time1) returns the inverse of "input_matrix" at time1
+# time1$get() returns the value of the "input_matrix"
 
 
-## Write a short comment describing this function
+# stores the "input_matrix" and compute the inverse of "input_matrix"
+makeCacheMatrix <- function(mtrx = matrix()) {
+  slvd <- NULL
+  set <- function(y) {
+    mtrx <<- y
+    slvd <<- NULL
+  }
+  get <- function() mtrx
+  setsolve <- function(solve) slvd <<- solve
+  getsolve <- function() slvd
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# returns the cached inverse matrix of "input_matrix"
+CacheSolve <- function(mtrx, ...) {
+  slvd <- mtrx$getsolve()
+  if(!is.null(slvd)) {
+    message("getting cached data")
+    return(slvd)
+  }
+  data <- mtrx$get()
+  slvd <- solve(data, ...)
+  mtrx$setsolve(slvd)
+  slvd
 }
